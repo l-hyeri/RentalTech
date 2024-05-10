f66570ccd193bdb7ad7ce419aa3e13d5c1f861a4
hyeri6318 <lhl6318@naver.com>
2024-05-10 22:41:08 +0900

    MySQL 연결 코드 오류 수정

--- src/main/java/rentaltech/electronics/ElectronicsApplication.java
+++ src/main/java/rentaltech/electronics/ElectronicsApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-//@SpringBootApplication
+@SpringBootApplication
 public class ElectronicsApplication {
 
 	public static void main(String[] args) {
