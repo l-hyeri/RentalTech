83cdb8c2b13817e4dabad18e06d1857da346d873
hyeri6318 <lhl6318@naver.com>
2024-05-23 23:34:39 +0900

    회원정보수정 service 구현

--- src/main/java/rentaltech/electronics/service/MemberService.java
+++ src/main/java/rentaltech/electronics/service/MemberService.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Service;
 import rentaltech.electronics.dto.MemberDto;
 import rentaltech.electronics.entity.Member;
 import rentaltech.electronics.repository.MemberRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -36,6 +38,26 @@ public class MemberService {
         } else {    // 조회 경로가 없는 경우
             return null;
         }
+    }
+
+    @Transactional(readOnly = true)
+    public MemberDto findByMail(String mail) {    // 회원정보 수정을 위한 이메일 조회
+        Optional<Member> findMember = memberRepository.findByMail(mail);
+
+        if (findMember.isPresent()) {
+            Member member = findMember.get();
+            MemberDto dto = MemberDto.toMemberDto(member);
+            return dto;
+        } else {
+            return null;
+        }
+    }
+
+    public void edit(MemberDto memberDto) {
+        Optional<Member> findMember = memberRepository.findByMail(memberDto.getMail());
+
+        Member member = findMember.get();
+        member.editMember(memberDto);
     }
 
     // 중복회원 확인 (회원가입)
