0fb45e7031de038b1bf2034532fe384b5fbe91cc
hyeri6318 <lhl6318@naver.com>
2024-07-09 15:02:57 +0900

    렌탈신청 목록 Repository 구현 추가

--- src/main/java/rentaltech/electronics/repository/RentalRepository.java
+++ src/main/java/rentaltech/electronics/repository/RentalRepository.java
@@ -1,7 +1,17 @@
 package rentaltech.electronics.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import rentaltech.electronics.entity.Rental;
 
+import java.util.List;
+
 public interface RentalRepository extends JpaRepository<Rental, Long> {
+
+    // 해당 유저의 구매 이력을 조회 (주문 객체)
+    @Query("select re from Rental re " +
+            "where re.member.mail = :email " +
+            "order by re.rentalDate desc")
+    List<Rental> findRentals(@Param("email") String email);
 }
