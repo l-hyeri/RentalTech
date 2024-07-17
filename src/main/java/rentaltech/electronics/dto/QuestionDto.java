2ae5107753439b21aad4070e6d1c45e5f4f3759f
hyeri6318 <lhl6318@naver.com>
2024-07-17 15:32:53 +0900

    상품 문의 작성 Entity, Dto 구현

---
+++ src/main/java/rentaltech/electronics/dto/QuestionDto.java
@@ -0,0 +1,14 @@
+package rentaltech.electronics.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QuestionDto {
+
+    private String title;
+    private String content;
+    private Long serialNum;
+
+}
---
+++ src/main/java/rentaltech/electronics/entity/Question.java
@@ -0,0 +1,45 @@
+package rentaltech.electronics.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "question_id")
+    private Long id;    // 문의사항 작성글 아이디
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "serialNum")
+    private Item item;
+
+    private String title;
+
+    private String content;
+
+    private String questionDate; // 글 작성 날짜
+
+    public static Question createQuestion(Member member, Item item, String title, String content) {
+        Question question = new Question();
+        question.setMember(member);
+        question.setItem(item);
+        question.setTitle(title);
+        question.setContent(content);
+        question.setQuestionDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+
+        return question;
+    }
+
+}
