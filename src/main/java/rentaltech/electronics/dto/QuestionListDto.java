47c021482592718fd5683b20d5b1e01522c01226
hyeri6318 <lhl6318@naver.com>
2024-07-18 14:10:31 +0900

    상품 문의 목록(사용자) Dto 구현

---
+++ src/main/java/rentaltech/electronics/dto/QuestionListDto.java
@@ -0,0 +1,25 @@
+package rentaltech.electronics.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import rentaltech.electronics.entity.Question;
+
+@Getter
+@Setter
+public class QuestionListDto {
+
+    private Long questionItemId;
+    private String questionDate;
+    private String title;
+    private String content;
+
+    public static QuestionListDto questionList(Question question) {
+        QuestionListDto dto = new QuestionListDto();
+        dto.setQuestionItemId(question.getItem().getSerialNum());
+        dto.setQuestionDate(question.getQuestionDate());
+        dto.setTitle(question.getTitle());
+        dto.setContent(question.getContent());
+
+        return dto;
+    }
+}
